c5fbcb6158e31c630d793cf8e018ab89f274061b
Ann Robles <annr.dev@gmail.com>
2023-01-12 12:08:00 -0500

    AAI-43 added dummy category and category navigation to notes/tasks list screen

---
+++ TaskAppsAssasinsIOS/Controller/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  TaskAppsAssasinsIOS
+//
+//  Created by Ann Robles on 1/12/23.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+}
---
+++ TaskAppsAssasinsIOS/Controller/NoteTaskSearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoteTaskSearchViewController.swift
+//  TaskAppsAssasinsIOS
+//
+//  Created by Ann Robles on 1/12/23.
+//
+
+import UIKit
+
+class NoteTaskSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
