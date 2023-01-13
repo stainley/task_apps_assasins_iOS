5e7a0a403454bd2f4e680885ec7b98848f4adac3
Stainley Lebron <saiyamans@gmail.com>
2023-01-12 23:50:21 -0500

    Data structure for note

--- TaskAppsAssasinsIOS/Model/Note.swift
+++ TaskAppsAssasinsIOS/Model/Note.swift
@@ -5,8 +5,44 @@
 //  Created by Stainley A Lebron R on 2023-01-12.
 //
 
-import Foundation
+import UIKit
 
 struct Note {
+    
+    private var title: String?
+    private var description: String?
+    private var creationDate: NSDate
+    private var pictures: [UIImage?]
+    private var audios: [String?]
+    
+    
+    init(title: String, description: String, creationDate: NSDate, pictures: [UIImage?], audios: [String?]) {
+        self.title = title
+        self.description = description
+        self.creationDate = creationDate
+        self.pictures = pictures
+        self.audios = audios
+    }
+    
+    
+    func getTitle() -> String? {
+        return self.title
+    }
+    
+    func getDescription() -> String? {
+        return self.description
+    }
+    
+    func getCreationDate() -> NSDate {
+        return self.creationDate
+    }
+    
+    func getPictures() -> [UIImage?] {
+        return self.pictures
+    }
+    
+    func getAudios() -> [String?] {
+        return self.audios
+    }
     
 }
