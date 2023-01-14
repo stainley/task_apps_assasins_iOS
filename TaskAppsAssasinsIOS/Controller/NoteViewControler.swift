35b016922e9d55de475cc3ca84768616e003cc4b
jayesh-khistria <115033392+jayesh-khistria@users.noreply.github.com>
2023-01-14 11:11:25 -0500

    added java class for notescreen and added select catagory dropdown

---
+++ TaskAppsAssasinsIOS/Controller/NoteViewControler.swift
@@ -0,0 +1,55 @@
+//
+//  NoteViewControler.swift
+//  TaskAppsAssasinsIOS
+//
+//  Created by Jayesh Khistria on 2023-01-13.
+//
+
+import UIKit
+
+class NoteViewControler: UIViewController {
+    
+    @IBOutlet weak var titleLbl: UITextField!
+    
+    @IBOutlet weak var catagoryBtn: UIButton!
+    
+    @IBOutlet weak var workBtn: UIButton!
+    
+    @IBOutlet weak var schoolBtn: UIButton!
+    
+    @IBOutlet weak var shoppingBtn: UIButton!
+    
+    @IBOutlet weak var groceryBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func selectCatagory(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    
+    
+    
+    
+
