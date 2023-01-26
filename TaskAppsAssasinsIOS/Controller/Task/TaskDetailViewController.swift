653ba79f1c3a5818c1e21a6a2af9c155df203d22
Stainley Lebron <saiyamans@gmail.com>
2023-01-26 10:52:13 -0500

    subtask binding

--- TaskAppsAssasinsIOS/Controller/Task/TaskDetailViewController.swift
+++ TaskAppsAssasinsIOS/Controller/Task/TaskDetailViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class TaskDetailViewController: UIViewController {
 
+    @IBOutlet weak var taskTitle: UITextField!
+    
     var task: TaskEntity?
+    weak var taskViewControllerDelegate: TaskViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        guard let task = task else {
+            return
+        }
+        taskTitle.text = task.title
     }
+    
+    @IBAction func addSubTaskButton(_ sender: UIButton) {
+        
+    }
+  
 
     
 }
